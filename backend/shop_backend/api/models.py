132b0de70540d38706f0e260e7472f49e316444d
Rozimurat <rozimuradmirpulatov101@gmail.com>
2025-12-01 21:28:38 +0500

    MODELS

--- backend/shop_backend/api/models.py
+++ backend/shop_backend/api/models.py
@@ -1,68 +1,86 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+
+#Custom User
 
 
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+
+#Category
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=200)
 
-    class Meta:
-        verbose_name_plural = "Categories"
-
-    def __str__(self):
+    def str(self):
         return self.name
 
-# ";vmksmvmsfvfm"
+
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    old_price = models.DecimalField(max_digits=10, decimal_places=2)
-    new_price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
 
-    def __str__(self):
-        return self.name
+    def str(self):
+        return self.title
 
+#Cart
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def str(self):
+        return f"Cart of {self.user.email}"
+
+
+#cartItem
 
 class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cart_items")
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
-    def subtotal(self):
-        return self.product.new_price * self.quantity
+    def str(self):
+        return f"{self.product.title} X {self.quantity}"
 
-    def __str__(self):
-        return f"{self.user.username} - {self.product.name} ({self.quantity})"
-
+#Order
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    status = models.CharField(max_length=20, default="Pending")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    class Meta:
-        ordering = ["-created_at"]
-
-    def __str__(self):
-        return f"Order #{self.id} - {self.user.username}"
-
-    def calculate_total(self):
-        total = sum(item.subtotal() for item in self.order_items.all())
-        self.total = total
-        self.save()
-        return total
+    def str(self):
+        return  f"Order #{self.id}"
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def subtotal(self):
-        return self.product.new_price * self.quantity
+    def str(self):
+        return f"{self.product.title} x {self.quantity}"
 
-    def __str__(self):
-        return f"{self.order} - {self.product.name} ({self.quantity})"
+
+
+
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    method = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+
+    def str(self):
+        return f"Payment for Order #{self.order.id}"
